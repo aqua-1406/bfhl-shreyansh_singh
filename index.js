5678c346e7256b06c1000e349f6a021da6770c6a
Shreyansh Singh <108362319+aqua-1406@users.noreply.github.com>
2024-08-25 11:56:54 +0530

    backend

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app
+  .route("/bfhl")
+  .get((req, res) => {
+    res.status(200).json({ operation_code: 1 });
+  })
+  .post((req, res) => {
+
+    const data = req.body.data || [];
+    const numbers = [];
+    const alphabets = [];
+    let highestLoweralphabet = "";
+
+  for (const item of data) {
+    if (!isNaN(item)) {
+      numbers.push(item);
+    } else if (item.length === 1 && isNaN(item)) {
+      if (item >= 'a' && item <= 'z') {
+        alphabets.push(item);
+        if (!highestLoweralphabet || item > highestLoweralphabet) {
+          highestLoweralphabet = item;
+        }
+      }
+    }
+  }
+
+    res.json({
+      is_success: true,
+      user_id: "shreyansh_kumar_singh_14062004",
+      email: "shreyanshkumar.singh2021@vitbhopal.ac.in",
+      roll_number: "21BEC10848",
+      numbers: numbers,
+      alphabets: alphabets,
+      highestLoweralphabet: highestLoweralphabet ? [highestLoweralphabet] : [],
+    });
+  });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
